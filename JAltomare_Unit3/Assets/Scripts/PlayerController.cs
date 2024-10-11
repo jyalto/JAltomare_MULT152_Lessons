16ce8ebd8d80d94ecdbadc967dbcb3eb532e071b
jyalto <161365560+jyalto@users.noreply.github.com>
2024-10-11 13:51:37 -0700

    Lesson3C

--- JAltomare_Unit3/Assets/Scripts/PlayerController.cs
+++ JAltomare_Unit3/Assets/Scripts/PlayerController.cs
@@ -10,20 +10,26 @@ public class PlayerController : MonoBehaviour
     private bool onGround = true;
     public bool gameOver = false;
 
+    private Animator animPlayer;
+
     void Start()
     {
-        rbPlayer = GetComponent<Rigidbody>(); //access and store the rigidboy once at start
+        rbPlayer = GetComponent<Rigidbody>(); //access and store the rigidbody
+        animPlayer = GetComponent<Animator>(); //access and store the player animator
         Physics.gravity *= gravityModifier;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         bool spaceDown = Input.GetKeyDown(KeyCode.Space);
-        if (spaceDown && onGround) 
+        // Conditions met to Jump
+        if (spaceDown && onGround && !gameOver) 
         {
             rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             onGround = false;
+            animPlayer.SetTrigger("Jump_trig");
         }
     }
     // resetting the value of on ground to true when the player reaches the ground
@@ -34,10 +40,13 @@ public class PlayerController : MonoBehaviour
         { 
             onGround = true; 
         }
+        // Game if over when this condition is met
         else if (collision.gameObject.CompareTag("Obstacle")) 
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            animPlayer.SetBool("Death_b", true);
+            animPlayer.SetInteger("DeathType_int", 2);
         }
     }
 }
