c9227adeb5fc2f2216e2d15735d577cc758ca837
jyalto <161365560+jyalto@users.noreply.github.com>
2024-10-31 21:34:30 -0700

    Unit4_Lesson C

--- JAltomare_Unit4/Assets/Scripts/PlayerController.cs
+++ JAltomare_Unit4/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,12 @@ public class PlayerController : MonoBehaviour
     private Renderer rendererPlayer;
     private GameObject focalPoint;
     public float speed = 10.0f;
+    public float powerUpSpeed = 10.0f;
+
+    public GameObject powerUpInd;
+
+    bool hasPowerUp = false; // This is a state variable
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +39,33 @@ public class PlayerController : MonoBehaviour
         {
             rendererPlayer.material.color = new Color(1.0f + forwardInput, 1.0f, 1.0f + forwardInput);
         }
+        powerUpInd.transform.position = transform.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountDown());
+            powerUpInd.SetActive(true);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (hasPowerUp && collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with " + collision.gameObject + " with power up set to: " + hasPowerUp);
+            Rigidbody rbEnemy = collision.gameObject.GetComponent<Rigidbody>(); // the collison gives us access to Enemy
+            Vector3 awayDir  = collision.gameObject.transform.position - transform.position;
 
+            rbEnemy.AddForce(awayDir * powerUpSpeed, ForceMode.Impulse);
+        }
+    }
+    IEnumerator PowerUpCountDown()
+    {
+        yield return new WaitForSeconds(8);
+        hasPowerUp = false;
+        powerUpInd.SetActive(false);
     }
 }
